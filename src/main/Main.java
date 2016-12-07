720c6a0c6c441b06cf2e396ca30127560681756c
Saraporn Viriyarojanakul <senaleron@gmail.com>
2016-12-07 20:08:24 +0700

    1 nothing important

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,19 +8,42 @@ import ui.GameOverScreen;
 import ui.GameScreen;
 import ui.StartScreen;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import logic.GameManager;
+import ui.GameOverScreen;
+import ui.GameScreen;
+import ui.StartScreen;
+import javafx.stage.WindowEvent;
+import javafx.event.EventHandler;
+
 public class Main extends Application {
 	private Stage primaryStage;
 	private Scene startScene;
-	private Scene GameOverScene;
-	private Scene GameScene;
-	private GameManager gamelogic;
+	private Scene overScene;
+	private Scene gameScene;
 	private GameScreen gameScreen;
 	private GameOverScreen overScreen;
 	private StartScreen startScreen;
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle(""); //What should we name this game *^*
+		this.primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>(){
+
+			@Override
+			public void handle(WindowEvent arg0) {
+				// TODO Auto-generated method stub
+				System.exit(0);
+			}
+			
+		});
 		
+	}
+	public static void main(String[] args) {
+		Application.launch(args);
 	}
 	
 }
