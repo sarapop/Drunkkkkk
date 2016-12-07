29ab18d4d078342423040ddd8826a4fe3a16fc1b
Saraporn Viriyarojanakul <senaleron@gmail.com>
2016-12-07 16:03:36 +0700

    เติมเมนนิดนึง

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,27 @@
 package main;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import logic.GameManager;
+import ui.GameOverScreen;
+import ui.GameScreen;
+import ui.StartScreen;
 
+public class Main extends Application {
+	private Stage primaryStage;
+	private Scene startScene;
+	private Scene GameOverScene;
+	private Scene GameScene;
+	private GameManager gamelogic;
+	private GameScreen gameScreen;
+	private GameOverScreen overScreen;
+	private StartScreen startScreen;
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
+
